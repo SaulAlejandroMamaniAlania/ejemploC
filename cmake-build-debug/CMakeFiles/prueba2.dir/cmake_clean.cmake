59fac77970bd666511808d76794eb912b88a7988
ALEJANDRO-777 <maxi_22_11@hotmail.com>
2018-05-30 23:00:41 -0300

    tipos de datos

---
+++ cmake-build-debug/CMakeFiles/prueba2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prueba2.dir/main.c.obj"
+  "prueba2.pdb"
+  "prueba2.exe"
+  "prueba2.exe.manifest"
+  "libprueba2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/prueba2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.1416
+int main()
+{
+    int x=10;
+    float suma=PI+x;
+    printf("El elemento es:%.3f",suma);
+    return 0;
+
+}
